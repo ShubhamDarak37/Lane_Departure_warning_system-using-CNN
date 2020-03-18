1f9c2db2416623e4e4e8193fd5605ae4d50dd367
shubbham37 <shubhamdarak37@gmail.com>
2020-03-18 10:03:50 +0530

    lane departure warning system

---
+++ draw_detected_lanes.py
@@ -0,0 +1,71 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+from scipy.misc import imresize
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+from tensorflow.keras.models import load_mode
+import matplotlib.pyplot as plt
+image_path="test1.png"
+image= cv2.imread(image_path)
+image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#cv2.polylines(img,[pts],True,(0,255,255),3)
+plt.imshow(image)
+# Load Keras model
+model = load_model('full_CNN_model.h5')
+
+# Class to average lanes with
+class Lanes():
+    def __init__(self):
+        self.recent_fit = []
+        self.avg_fit = []
+
+def road_lines(image):
+    """ Takes in a road image, re-sizes for the model,
+    predicts the lane to be drawn from the model in G color,
+    recreates an RGB image of a lane and merges with the
+    original road image.
+    """
+    
+    # Get image ready for feeding into model
+    image = image[700:1080,400:1600]
+    
+    small_img = imresize(image, (80, 160, 3))
+    small_img = np.array(small_img).astype(np.float32)
+    small_img = small_img[None,:,:,:]
+
+    # Make prediction with neural network (un-normalize value by multiplying by 255)
+    prediction = model.predict(small_img)[0] * 255
+
+    # Add lane prediction to list for averaging
+    lanes.recent_fit.append(prediction)
+    # Only using last five for average
+    if len(lanes.recent_fit) > 5:
+        lanes.recent_fit = lanes.recent_fit[1:]
+
+    # Calculate average detection
+    lanes.avg_fit = np.mean(np.array([i for i in lanes.recent_fit]), axis = 0)
+
+    # Generate fake R & B color dimensions, stack with G
+    blanks = np.zeros_like(prediction).astype(np.uint8)
+    lane_drawn = np.dstack((blanks, prediction, blanks))
+
+    # Re-size to match the original image
+    lane_image = imresize(lane_drawn, (380, 1200, 3))
+    plt.imshow(lane_image)
+    
+    # Merge the lane drawing onto the original image
+    result = cv2.addWeighted(image, 1, lane_image, 1, 0)
+
+    return result
+
+lanes = Lanes()
+
+# Where to save the output video
+vid_output = 'proj_reg_vid.mp4'
+
+# Location of the input video
+clip1 = VideoFileClip("project_new.mp4")
+
+vid_clip = clip1.fl_image(road_lines)
+vid_clip.write_videofile(vid_output, audio=False)
